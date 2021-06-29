e6a0a92b47e7b17545ce0536c03ce32e7c279b42
Leo Gallucci <elgalu3@gmail.com>
2021-06-29 18:47:23 +0200

    Bump to 0.0.1

--- slurmops/__init__.py
+++ slurmops/__init__.py
@@ -5,7 +5,7 @@
 :license: Apache 2.0, see LICENSE for more details.
 """
 # Do not change the version here but rather `tbump "0.0.0.1" --only-patch`
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 # `__all__` is left here for documentation purposes and as a
 # reference to which interfaces are meant to be imported.
