adbec95ca62e3e1f827745388ed396fce38075eb
Leo Gallucci <elgalu3@gmail.com>
2021-06-29 21:26:44 +0200

    [ops] after release merge to main
    Signed-off-by: Leo Gallucci <elgalu3@gmail.com>

---
+++ scripts/merge_to_main.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+# Purpose: Merge current branch into main
+#
+
+# set -e: exit asap if a command exits with a non-zero status
+set -e
+
+# set -o pipefail: Don't mask errors from a command piped into another command
+set -o pipefail
+
+# set -x: prints all lines before running debug (debugging)
+set -x
+
+# set -u: treat unset variables as an error and exit immediately
+set -u
+
+_VERSION="$(python -c 'from slurmops import __version__; print(__version__)')"
+
+git checkout "main"
+git merge "release-${_VERSION}"
+git push
+git branch --delete "release-${_VERSION}"
--- slurmops/__init__.py
+++ slurmops/__init__.py
@@ -4,7 +4,7 @@
 :copyright: (c) 2021 by Leo Gallucci.
 :license: Apache 2.0, see LICENSE for more details.
 """
-# Do not change the version here but rather `tbump "0.0.0.1" --only-patch`
+# Do not change the version here but rather `tbump "0.0.2" --only-patch`
 __version__ = "0.0.1"
 
 # `__all__` is left here for documentation purposes and as a
